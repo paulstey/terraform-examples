2a12203d1ccfbb232d54ecd6cb873a2ae73550e5
Paul Stey <paulstey@gmail>
2022-09-05 11:04:06 -0400

    first commit

---
+++ simple_vm_and_vpc/env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export GOOGLE_APPLICATION_CREDENTIALS="${PWD}/secrets/svc_account_creds.json"
---
+++ simple_vm_and_vpc/main.tf
@@ -0,0 +1,29 @@
+# main.tf
+
+provider "google" {
+  project     = "ccv-oscar-utilization"
+  region      = "us-east4"
+  zone        = "us-east4-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link 
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
